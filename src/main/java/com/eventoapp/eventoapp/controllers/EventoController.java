403349e3b8c6e3fb5cb612cd8b4af5d38dd703a7
pablo gonzalez <p-gonzalez@bol.com.br>
2021-06-15 03:01:28 -0300

    criando validação de dados

--- src/main/java/com/eventoapp/eventoapp/controllers/EventoController.java
+++ src/main/java/com/eventoapp/eventoapp/controllers/EventoController.java
@@ -1,13 +1,15 @@
 package com.eventoapp.eventoapp.controllers;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-
-
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.eventoapp.eventoapp.models.Convidado;
 import com.eventoapp.eventoapp.models.Evento;
@@ -39,6 +41,7 @@ public class EventoController {
 		mv.addObject("eventos", eventos);
 		return mv;
 	}
+	
 	/*@RequestMapping("/{codigo}")
 	public ModelAndView detalhesEvento(@PathVariable("codigo") long codigo) {
 		Evento evento = er.findByCodigo(codigo);
@@ -47,19 +50,26 @@ public class EventoController {
 		return mv;
 	}
 	*/
+	
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
 	public ModelAndView detalhesEvento(@PathVariable("id") long id){
 		Evento evento = er.findById(id);
 		ModelAndView mv = new ModelAndView("evento/detalhesEvento");
 		mv.addObject("evento", evento);
+		Iterable<Convidado>convidados = cr.findByEvento(evento);
+		mv.addObject("convidados", convidados);
 		return mv;
 	}
 	@RequestMapping(value="/{id}", method=RequestMethod.POST)
-	public String detalhesEvento(@PathVariable("id") long id, Convidado convidado ) {
+	public String detalhesEventoPost(@PathVariable("id") long id,@Valid Convidado convidado, BindingResult result, RedirectAttributes attributes) {
+		if(result.hasErrors()) {
+		attributes.addFlashAttribute("mensagem", "Verifique os campos");	
+		return "redirect:/{id}";
+		}
 		Evento evento = er.findById(id);
 		convidado.setEvento(evento);
-		
 		cr.save(convidado);
+		attributes.addFlashAttribute("mensagem", "Convidado adicionado com sucesso!");
 		return "redirect:/{id}";
 	}
 }
--- src/main/java/com/eventoapp/eventoapp/models/Convidado.java
+++ src/main/java/com/eventoapp/eventoapp/models/Convidado.java
@@ -4,10 +4,14 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 
+import org.hibernate.validator.constraints.NotEmpty;
+
 @Entity
 public class Convidado {
 	@Id
+	@NotEmpty
 	private String rg;
+	@NotEmpty
 	private String nomeConvidado;
 	
 	@ManyToOne
